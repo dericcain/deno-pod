f52eeb14f250c6156008453d82b2a3992b5b80c7
Deric Cain <deric@shipt.com>
2022-10-15 07:55:17 -0500

    rtrtrtrt (#1) (#2)

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,5 @@
 import { PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
+export default function Greet(props: PageProps<{ name: string }>) {
   return <div>Hello {props.params.name}</div>;
 }
